c8760c498dba7d16726e3d11e09360cf4cd2a723
Luz_2_Profile <est.juliana.bonillac@unimilitar.edu.co>
2025-11-12 17:20:14 -0500

    Brillo

--- SYNESTHESIA ROOM/Assets/Codigos/Control_Volumen.cs
+++ SYNESTHESIA ROOM/Assets/Codigos/Control_Volumen.cs
@@ -8,12 +8,20 @@ public class Control_Volumen : MonoBehaviour
     public float sliderValor;
     public Image imagenMute;
 
+    public Slider sliderBrillo;
+    public float sliderBrilloValor;
+    public Image panelBrillo;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         slider.value = PlayerPrefs.GetFloat("VolumenAudio", 0.5f);
         AudioListener.volume = slider.value;
         RevisarSiEstoyMute();
+
+        sliderBrillo.value = PlayerPrefs.GetFloat("BrilloPantalla", 0.5f);
+        panelBrillo.color = new Color(panelBrillo.color.r,panelBrillo.color.g,panelBrillo.color.b,sliderBrillo.value);
 
 
     }
@@ -36,6 +44,12 @@ public class Control_Volumen : MonoBehaviour
         {
             imagenMute.enabled = false;
         }
+    }
+    public void CambiarBrillo(float valor)
+    {
+        sliderBrilloValor = valor;
+        PlayerPrefs.SetFloat("BrilloPantalla", sliderBrilloValor);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, sliderBrillo.value);
     }
 
     // Update is called once per frame
