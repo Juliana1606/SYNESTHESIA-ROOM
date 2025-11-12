a514b06d42385bc1823b3f05a78af053b09244be
Luz_2_Profile <est.juliana.bonillac@unimilitar.edu.co>
2025-11-12 17:13:50 -0500

    Funcionamiento_Volumen

--- SYNESTHESIA ROOM/Assets/Codigos/Control_Volumen.cs
+++ SYNESTHESIA ROOM/Assets/Codigos/Control_Volumen.cs
@@ -1,11 +1,41 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 public class Control_Volumen : MonoBehaviour
 {
+    public Slider slider;
+    public float sliderValor;
+    public Image imagenMute;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider.value = PlayerPrefs.GetFloat("VolumenAudio", 0.5f);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+
+
+    }
+    public void CambiarSlider(float valor)
+    {
+
+        sliderValor = valor;
+        PlayerPrefs.SetFloat("VolumenAudio", sliderValor);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        if (slider.value == 0)
+        {
+            imagenMute.enabled = true;
+        }
+        else
+        {
+            imagenMute.enabled = false;
+        }
     }
 
     // Update is called once per frame
